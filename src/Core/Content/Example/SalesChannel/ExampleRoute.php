b526c115e688fde2e59ba3af0fa1d3f780e5b6a2
Ihor Tarknaiev <13971793it@gmail.com>
2024-12-03 20:16:09 +0100

    add api

---
+++ src/Core/Content/Example/SalesChannel/ExampleRoute.php
@@ -0,0 +1,25 @@
+<?php declare(strict_types=1);
+
+namespace Ihor\Frame2\Core\Content\Example\SalesChannel;
+
+use Shopware\Core\Content\Product\ProductEntity;
+use Shopware\Core\Framework\DataAbstractionLayer\EntityRepository;
+use Shopware\Core\Framework\DataAbstractionLayer\Search\Criteria;
+use Shopware\Core\Framework\Plugin\Exception\DecorationPatternException;
+use Shopware\Core\System\SalesChannel\SalesChannelContext;
+use Symfony\Component\Routing\Attribute\Route;
+
+#[Route(defaults: ['_routeScope' => ['store-api']])]
+class ExampleRoute extends AbstractExampleRoute
+{
+    public function getDecorated(): AbstractExampleRoute
+    {
+        throw new DecorationPatternException(self::class);
+    }
+
+    #[Route(path: '/store-api/example', name: 'store-api.example.search', defaults: ['_entity' => 'product'], methods: ['GET','POST'])]
+    public function load(Criteria $criteria, SalesChannelContext $context): ExampleRouteResponse
+    {
+        return new ExampleRouteResponse(new ProductEntity());
+    }
+}
---
+++ src/Storefront/Controller/ExampleController.php
@@ -0,0 +1,25 @@
+<?php declare(strict_types=1);
+
+namespace Ihor\Frame2\Storefront\Controller;
+
+use Ihor\Frame2\Core\Content\Example\SalesChannel\AbstractExampleRoute;
+use Shopware\Core\Framework\DataAbstractionLayer\Search\Criteria;
+use Shopware\Core\System\SalesChannel\SalesChannelContext;
+use Shopware\Storefront\Controller\StorefrontController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+#[Route(defaults: ['_routeScope' => ['storefront']])]
+class ExampleController extends StorefrontController
+{
+    public function __construct(
+        private readonly AbstractExampleRoute $route
+    ) {
+    }
+
+    #[Route(path: '/example', name: 'frontend.example.search', defaults: ['XmlHttpRequest' => 'true', '_entity' => 'product'], methods: ['GET', 'POST'])]
+    public function load(Criteria $criteria, SalesChannelContext $context): Response
+    {
+        return $this->route->load($criteria, $context);
+    }
+}
